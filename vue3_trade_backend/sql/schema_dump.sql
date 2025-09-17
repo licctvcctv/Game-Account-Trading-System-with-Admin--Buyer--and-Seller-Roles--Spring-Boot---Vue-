24d7a605a8e3848a446bbe39eb05e7b6a4efdf3c
licctvcctv <107204922+licctvcctv@users.noreply.github.com>
2025-09-17 08:00:49 +0800

    feat: Add database schema and data dumps

---
+++ vue3_trade_backend/sql/schema_dump.sql
@@ -0,0 +1,339 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Linux (aarch64)
+--
+-- Host: localhost    Database: trade
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `barrage`
+--
+
+DROP TABLE IF EXISTS `barrage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `barrage` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `message` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '弹幕文本',
+  `userAvatar` varchar(1024) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '用户头像',
+  `userId` bigint NOT NULL COMMENT '用户id',
+  `isSelected` tinyint NOT NULL DEFAULT '0' COMMENT '是否精选（默认0，精选为1）',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1890696292486328323 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '评论 ID',
+  `postId` bigint NOT NULL COMMENT '面经帖子 ID',
+  `userId` bigint NOT NULL COMMENT '用户 ID',
+  `content` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '评论内容',
+  `parentId` bigint DEFAULT NULL COMMENT '父评论 ID，支持多级嵌套回复',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+  `ancestorId` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `comment_questionId` (`postId`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1890696721316163587 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `commodity`
+--
+
+DROP TABLE IF EXISTS `commodity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commodity` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品 ID',
+  `commodityName` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '商品名称',
+  `commodityDescription` varchar(2048) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '商品简介',
+  `commodityAvatar` varchar(1024) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '商品封面图',
+  `degree` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '商品新旧程度（例如 9成新）',
+  `commodityTypeId` bigint DEFAULT NULL COMMENT '商品分类 ID',
+  `tradeType` tinyint DEFAULT '1' COMMENT '1-å‡ºå”® 2-å‡ºç§Ÿ',
+  `adminId` bigint NOT NULL COMMENT '管理员 ID （某人创建该商品）',
+  `isListed` tinyint DEFAULT '0' COMMENT '是否上架（默认0未上架，1已上架）',
+  `commodityInventory` int DEFAULT '0' COMMENT '商品数量（默认0）',
+  `price` decimal(10,2) NOT NULL COMMENT '商品价格',
+  `viewNum` int DEFAULT '0' COMMENT '商品浏览量',
+  `favourNum` int DEFAULT '0' COMMENT '商品收藏量',
+  `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `type_index` (`commodityTypeId`) USING BTREE,
+  KEY `name_index` (`commodityName`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1968097621595701251 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `commodity_order`
+--
+
+DROP TABLE IF EXISTS `commodity_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commodity_order` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '订单 ID',
+  `userId` bigint NOT NULL COMMENT '用户 ID',
+  `sellerId` bigint DEFAULT NULL,
+  `commodityId` bigint NOT NULL COMMENT '商品 ID',
+  `remark` varchar(1024) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '订单备注',
+  `buyNumber` int DEFAULT NULL COMMENT '购买数量',
+  `paymentAmount` decimal(10,2) DEFAULT NULL COMMENT '订单总支付金额',
+  `payStatus` tinyint DEFAULT '0' COMMENT '0-未支付 1-已支付',
+  `deliveryStatus` tinyint DEFAULT '0',
+  `deliveryContent` varchar(2048) DEFAULT NULL,
+  `deliverTime` datetime DEFAULT NULL,
+  `finishStatus` tinyint DEFAULT '0',
+  `finishTime` datetime DEFAULT NULL,
+  `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1967957600039514114 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `commodity_score`
+--
+
+DROP TABLE IF EXISTS `commodity_score`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commodity_score` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品评分 ID',
+  `commodityId` bigint NOT NULL COMMENT '商品 ID',
+  `userId` bigint NOT NULL COMMENT '用户 ID',
+  `score` int NOT NULL COMMENT '评分（0-5，星级评分）',
+  `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间\r\n',
+  `updateTime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `scoreId` (`commodityId`,`userId`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1967509283872460803 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `commodity_type`
+--
+
+DROP TABLE IF EXISTS `commodity_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commodity_type` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品分类 ID',
+  `typeName` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '商品类别名称',
+  `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1967899536701526018 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `notice`
+--
+
+DROP TABLE IF EXISTS `notice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notice` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `noticeTitle` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '公告标题',
+  `noticeContent` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '公告内容',
+  `noticeAdminId` bigint NOT NULL COMMENT '创建人id（管理员）',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1967474664615792643 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `title` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标题',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '内容',
+  `tags` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标签列表（json 数组）',
+  `thumbNum` int NOT NULL DEFAULT '0' COMMENT '点赞数',
+  `favourNum` int NOT NULL DEFAULT '0' COMMENT '收藏数',
+  `userId` bigint NOT NULL COMMENT '创建用户 id',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `idx_userId` (`userId`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1967474747444908035 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='帖子';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `post_favour`
+--
+
+DROP TABLE IF EXISTS `post_favour`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_favour` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `postId` bigint NOT NULL COMMENT '帖子 id',
+  `userId` bigint NOT NULL COMMENT '创建用户 id',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `idx_postId` (`postId`) USING BTREE,
+  KEY `idx_userId` (`userId`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=184 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC COMMENT='帖子收藏';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `post_thumb`
+--
+
+DROP TABLE IF EXISTS `post_thumb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_thumb` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `postId` bigint NOT NULL COMMENT '帖子 id',
+  `userId` bigint NOT NULL COMMENT '创建用户 id',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `idx_postId` (`postId`) USING BTREE,
+  KEY `idx_userId` (`userId`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=178 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC COMMENT='帖子点赞';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `private_message`
+--
+
+DROP TABLE IF EXISTS `private_message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `private_message` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '消息 ID',
+  `senderId` bigint NOT NULL COMMENT '发送者 ID',
+  `recipientId` bigint NOT NULL COMMENT '接收者 ID',
+  `content` varchar(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '消息内容(UTF8MB4 支持Emoji表情)',
+  `alreadyRead` tinyint DEFAULT '0' COMMENT '0-未阅读 1-已阅读',
+  `type` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '消息发送类型（用户发送还是管理员发送,user Or admin)枚举',
+  `isRecalled` tinyint DEFAULT '0' COMMENT '是否撤回  0-未撤回 1-已撤回',
+  `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1967474490111774723 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `userAccount` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '账号',
+  `userPassword` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
+  `unionId` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '微信开放平台id',
+  `mpOpenId` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '公众号openId',
+  `userName` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户昵称',
+  `userAvatar` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户头像',
+  `userProfile` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户简介',
+  `userRole` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user' COMMENT '用户角色：user/admin/ban',
+  `userPhone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '联系电话',
+  `sellPermission` tinyint DEFAULT '0',
+  `rentPermission` tinyint DEFAULT '0',
+  `sellApplyStatus` tinyint DEFAULT '0',
+  `rentApplyStatus` tinyint DEFAULT '0',
+  `aiRemainNumber` int DEFAULT '0' COMMENT '用户 AI 剩余可使用次数',
+  `balance` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '用户余额（仅AI接口调用）',
+  `editTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '编辑时间',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `idx_unionId` (`unionId`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1968088237431898115 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='用户';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_ai_message`
+--
+
+DROP TABLE IF EXISTS `user_ai_message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_ai_message` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `userInputText` varchar(4096) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '用户输入',
+  `aiGenerateText` varchar(4096) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'AI 生成结果',
+  `userId` bigint NOT NULL COMMENT '用户ID',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '逻辑删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1901156016193175555 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_commodity_favorites`
+--
+
+DROP TABLE IF EXISTS `user_commodity_favorites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_commodity_favorites` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `userId` bigint NOT NULL COMMENT '用户 ID',
+  `commodityId` bigint NOT NULL COMMENT '商品 ID',
+  `status` tinyint DEFAULT '1' COMMENT '1-正常收藏 0-取消收藏',
+  `remark` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '用户备注',
+  `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `unique_favorite` (`userId`,`commodityId`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1967957478014627843 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-16 23:57:36
